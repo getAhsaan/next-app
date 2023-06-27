2b09b2c9b1314154bbc79af1c721bb035571eec4
getahsaan <getahsaan@gmail.com>
2023-06-27 17:31:47 +0600

    ssr

--- src/app/blogs/[id]/page.js
+++ src/app/blogs/[id]/page.js
@@ -2,15 +2,14 @@
 import { useParams, useSearchParams } from "next/navigation";
 import React from "react";
 
-const SingleBlog = ({ params, searchParams }) => {
+const SingleBlog = ({ params}) => {
 
-  const [year, id] = params.segments || [];
-  const params2 = useParams();
-  const searchParams2 = useSearchParams();
+  // const params2 = useParams();
+  // const searchParams2 = useSearchParams();
   // console.log(params2.segments.split("/"), searchParams2.get("title"));
   return (
     <div>
-      SingleBlog of {year || new Date().getFullYear()} for {id || 1}
+      SingleBlog  {params.id}
     </div>
   );
 };
--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -3,47 +3,29 @@ import Link from "next/link";
 // import { useRouter } from "next/navigation";
 import React from "react";
 
-const blogs = [
-  {
-    id: 1,
-    year: 2016,
-    title: "title 1",
-  },
-  {
-    id: 2,
-    year: 2017,
-    title: "title 2",
-  },
-  {
-    id: 3,
-    year: 2018,
-    title: "title 3",
-  },
-  {
-    id: 4,
-    year: 2019,
-    title: "title 4",
-  },
-];
-
-const BlogsPage = () => {
+const BlogsPage = async () => {
   // const router = useRouter()
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    cache: "force-cache",
+  });
+  const blogs = await res.json();
   return (
     <div className="container mx-auto">
-      {blogs.map(({ id, year, title }) => (
-        <Link
-          className="block border border-blue-500 p-2 my-2"
-          key={id}
-          href={{
-            pathname: `blogs/${year}/${id}`,
-            query: {
-              title: title,
-            },
-          }}
-          // onClick={() => router.push(`blogs/${year}/${id}?title=${title}`)}
-        >
-          {title}
-        </Link>
+      {blogs.map(({ id, title, body }) => (
+        <div key={id} className="block border border-blue-500 p-2 my-2">
+          <h2 className="text-xl">
+            {id}.{title}
+          </h2>
+          <p>{body}</p>
+          <Link className="inline-block mt-5"
+            href={`/blogs/${id}`}
+            // onClick={() => router.push(`blogs/${year}/${id}?title=${title}`)}
+          >
+            <button className="btn text-white border p-2 m-1 bg-blue-500 rounded-lg text-sm">
+              Details
+            </button>
+          </Link>
+        </div>
       ))}
     </div>
   );
